cdecefafb7d9cfab80816362044607a7a71f10c7
Eric Asante <3ericasante@gmail.com>
2025-07-14 14:57:48 -0400

    card change

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79506cea0823f1b646749971c1e2c8082346fea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca1b73363f506ba1420571da8f4f6653b5aea04")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
